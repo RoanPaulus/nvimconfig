283522020394bc48d5780d9b46315dd3021a8841
Roan Paulus <roan.paulus@amsterdam.com>
2024-05-13 19:44:00 +0200

    new

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -404,6 +404,9 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
+		opts = {
+			no_italic = true,
+		},
 		init = function()
 			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
