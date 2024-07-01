970758933901fd4d3d58c4c0f8361af7f0beccc2
Roan Paulus <roan.paulus@amsterdam.com>
2024-07-01 22:38:25 +0200

    added new plugins

---
+++ lua/plugins/dadbod.lua
@@ -0,0 +1,6 @@
+-- [[ Database connection UI (:DB...) ]]
+return {
+	"tpope/vim-dadbod",
+	"kristijanhusak/vim-dadbod-completion",
+	"kristijanhusak/vim-dadbod-ui",
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,6 +23,10 @@ return {
 		},
 	},
 	config = function()
+		vim.diagnostic.config({
+			underline = false,
+		})
+
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 			callback = function(event)
@@ -77,10 +81,6 @@ return {
 
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
 				if client and client.server_capabilities.documentHighlightProvider then
-					-- map("<leader>h", function()
-					-- 	vim.lsp.buf.document_highlight()
-					-- end, "[H]ighlight words/symbols under cursor in document")
-
 					-- [[ Autocmd version of the above ]]
 					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 						buffer = event.buf,
